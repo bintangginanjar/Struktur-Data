3613a4374d807ce7bb9ab56c7e01206053d91689
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-03-29 15:06:37 +0700

    Create main.cpp

---
+++ Chapter 2 - Abstract Data Type/Contoh ADT/main.cpp
@@ -0,0 +1,23 @@
+#include "jam.h"
+
+int main() {
+    tJam jam;
+
+    jam.setJam(12, 45, 10);
+    jam.cetakJam();
+
+    /*
+    cout << "Jam: ";
+    cin >> jam.hh;
+
+    cout << "Menit: ";
+    cin >> jam.mm;
+
+    cout << "Detik: ";
+    cin >> jam.ss;
+    */
+
+    //cout << "Jam sekarang " << jam.hh << ":" << jam.mm << ":" << jam.ss << endl;
+    //cout << "Hello world!" << endl;
+    return 0;
+}

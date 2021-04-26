4b65199bc77b0b383ce467a87a30fb59bcbc5ec5
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-04-26 14:10:07 +0700

    Update main.cpp

--- Chapter 6 - Single Linked List Circular Part 1 - SLLC with Head/main.cpp
+++ Chapter 6 - Single Linked List Circular Part 1 - SLLC with Head/main.cpp
@@ -22,132 +22,134 @@ int isEmpty() {
 }
 
 void insertDepan(int dataBaru) {
-    TNode *nodeBaru, *nodeBantu;
+    TNode *nodeBaru, *nodeBantu;            // step 1. inisialisasi nodeBaru dan nodeBantu
     nodeBaru = new TNode;
 
     nodeBaru->data = dataBaru;
     nodeBaru->next = NULL;
+                                            // cek apakah SLLC kosong?
+    if (isEmpty() == 1) {                   // step 2a. SLLLC masih kosong
+        head = nodeBaru;                    // step 3a. pointer head di-assign kepada nodeBaru
+        head->next = head;                  // step 4a. next pointer diarahkan kepada head
+    } else {                                // step 2b. SLLC sudah ada node (tidak kosong)
+        nodeBantu = head;                   // step 3b. pointer nodeBantu diassign ke head
 
-    if (isEmpty() == 1) {
-        head = nodeBaru;
-        head->next = head;
-    } else {
-        nodeBantu = head;
-        while (nodeBantu->next != head) {
+        while (nodeBantu->next != head) {   // step 4b. cari node terakhir dengan acuan next != head
             nodeBantu = nodeBantu->next;
         }
-        nodeBaru->next = head;
-        head = nodeBaru;
-        nodeBantu->next = head;
+        nodeBaru->next = head;              // step 5b. keluar looping, assign next nodeBaru kepada head
+        head = nodeBaru;                    // step 6b. assign head kepada nodeBaru
+        nodeBantu->next = head;             // step 7b. assign next untuk node terakhir kepada head yang baru
     }
 
     cout << "Data " << dataBaru << " masuk sebagai node paling depan" << endl;
 }
 
 void insertBelakang(int dataBaru) {
-    TNode *nodeBaru, *nodeBantu;
+    TNode *nodeBaru, *nodeBantu;            // step 1. inisialisasi nodeBaru dan nodeBantu
     nodeBaru = new TNode;
 
     nodeBaru->data = dataBaru;
     nodeBaru->next = nodeBaru;
+                                            // cek apakah SLLC masih kosong
+    if (isEmpty() == 1) {                   // step 2a. SLLLC masih kosong
+        head = nodeBaru;                    // step 3a. pointer head di-assign kepada nodeBaru
+        head->next = head;                  // step 4a. next pointer diarahkan kepada head
+    } else {                                // step 2b. SLLC sudah ada node (tidak kosong)
+        nodeBantu = head;                   // step 3b. pointer nodeBantu diassign ke head
 
-    if (isEmpty() == 1) {
-        head = nodeBaru;
-        head->next = head;
-    } else {
-        nodeBantu = head;
-
-        while (nodeBantu->next != head) {
+        while (nodeBantu->next != head) {   // step 4b. cari node terakhir dengan acuan next != head
             nodeBantu = nodeBantu->next;
         }
 
-        nodeBantu->next = nodeBaru;
-        nodeBaru->next = head;
+        nodeBantu->next = nodeBaru;         // step 5b. keluar looping, assign next nodeBantu kepada nodeBaru
+        nodeBaru->next = head;              // step 6b. assign next nodeBaru kepada head
     }
 
     cout << "Data " << dataBaru << " masuk sebagai node paling belakang" << endl;
 }
 
 void hapusDepan() {
-    TNode *nodeHapus, *nodeBantu;
+    TNode *nodeHapus, *nodeBantu;               // step 1. inisialisasi nodeHapus dan nodeBantu
     int d;
-
-    if (isEmpty() == 0) {
-        nodeHapus = head;
+                                                // cek apakah SLLC masih kosong
+    if (isEmpty() == 0) {                       // step 2a. SLLC tidak kosong
+        nodeHapus = head;                       // step 3a. assign nodeHapus kepada head
         d = nodeHapus->data;
+                                                // cek apakah SLLC hanya berisi satu node
+        if (head->next != head) {               // step 4a.a SLLC berisi lebih dari satu node
+            nodeBantu = head;                   // step 5a.b assign nodeBantu kepada head
 
-        if (head->next != head) {
-            nodeBantu = head;
-
-            while (nodeBantu->next != head) {
-                nodeBantu = nodeBantu->next;
+            while (nodeBantu->next != head) {   // step 6a.b selama next dari nodeBantu != head
+                nodeBantu = nodeBantu->next;    // step 7a.b pindahkan nodeBantu ke node selanjutnya
             }
-
-            head = head->next;
-            delete nodeHapus;
-            nodeBantu->next = head;
+                                                // keluar looping
+            head = head->next;                  // step 8a.b pindahkan head ke node selanjutnya
+            nodeBantu->next = head;             // step 9a.b assign next dari node terakhir ke head yang baru
+            delete nodeHapus;                   // step 10a.b hapus head yang lama
         } else {
-            head = NULL;
+            head = NULL;                        // step 4b.a SLLC berisi hanya satu node, maka head kita assign dengan NULL
         }
         cout << "Data " << d << " terhapus" << endl;
-    } else {
+    } else {                                    // SLLC masih kosong
         cout << "Linked List masih kosong\n";
     }
 }
 
 void hapusBelakang() {
-    TNode *nodeHapus,*nodeBantu;
+    TNode *nodeHapus,*nodeBantu;                // step 1. inisialisasi nodeHapus dan nodeBantu
     int d;
-
-    if (isEmpty() == 0) {
-        nodeHapus = head;
-
+                                                // cek apakah SLLC masih kosong
+    if (isEmpty() == 0) {                       // step 2a. SLLC tidak kosong
+        nodeHapus = head;                       // step 3a. assign nodeHapus kepada head
+                                                // cek apakah SLLC hanya berisi satu node
         if (head->next == head) {
             d = nodeHapus->data;
-            head = NULL;
-        } else {
-            nodeBantu = head;
+            head = NULL;                        // step 4a.a SLLC hanya berisi satu node maka head kita assign dengan NULL
+        } else {                                // step 4a.b SLLC berisi lebih dari satu node
+            nodeBantu = head;                       // step 5a.b assign nodeBantu kepada
 
-            while (nodeBantu->next->next != head) {
-                nodeBantu = nodeBantu->next;
+            while (nodeBantu->next->next != head) { // step 6a.b selama next dari next-nya nodeBantu != head
+                nodeBantu = nodeBantu->next;        // step 7a.b pindahkan nodeBantu ke node berikutnya
             }
-
-            nodeHapus = nodeBantu->next;
+                                                    // keluar looping, nodeBantu sudah berada di posisi node kedua terakhir
+            nodeHapus = nodeBantu->next;            // step 8a.b assign nodeHapus kepada next dari nodeBantu
             d = nodeHapus->data;
-            nodeBantu->next = head;
-            delete nodeHapus;
+            nodeBantu->next = head;                 // step 9a.b assign next dari nodeBantu ke head
+            delete nodeHapus;                       // step 10.a hapus node terakhir
         }
         cout << "Data " << d << " terhapus" << endl;
     } else {
-        cout << "Linked List masih kosong\n";
+        cout << "Linked List masih kosong\n";   // step 2b. SLLC masih kosong
     }
 }
 
 void hapusSemua() {
-    TNode *nodeBantu, *nodeHapus;
-    nodeBantu = head;
+    TNode *nodeBantu, *nodeHapus;       // step 1. inisialisasi nodeHapus dan nodeBantu
+    nodeBantu = head;                   // step 2. nodeBantu di assign kepada head
 
-    while (nodeBantu->next != head) {
-        nodeHapus = nodeBantu;
-        nodeBantu = nodeBantu->next;
-        delete nodeHapus;
+    while (nodeBantu->next != head) {   // step 3. selama next dari nodeBantu != head
+        nodeHapus = nodeBantu;          // step 4. assign nodeHapus kepada nodeBantu
+        nodeBantu = nodeBantu->next;    // step 5. pindahkan nodeBantu ke node selanjutnya
+        delete nodeHapus;               // step 6. hapus node
     }
-    head = NULL;
+                                        // keluar looping, nodeBantu di posisi terakhir
+    head = NULL;                        // step 7. assign head dengan NULL
     cout << "Linked List kosong\n";
 }
 
 void tampilData() {
-    TNode *nodeBantu;
+    TNode *nodeBantu;                           // step 1. inisialisasi nodeBantu
 
-    nodeBantu = head;
-
-    if (isEmpty() == 0) {
+    nodeBantu = head;                           // step 2. assign nodeBantu kepada head
+                                                // cek apakah SLLC masih kosong
+    if (isEmpty() == 0) {                       // step 3a. SLLC tidak kosong
         do {
-            cout << nodeBantu->data << endl;
-            nodeBantu = nodeBantu->next;
+            cout << nodeBantu->data << endl;    // step 4a. cetak data
+            nodeBantu = nodeBantu->next;        // step 5a. pindahkan nodeBantu ke node selanjutnya selama nodeBantu bukan head
         } while (nodeBantu != head);
     } else {
-        cout << "List masih kosong" << endl;
+        cout << "List masih kosong" << endl;    // step 3b. SLLC masih kosong
     }
 }
 

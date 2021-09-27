2fe7844b8da1f762e9532f654152209c27aaa863
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-09-27 22:43:36 +0700

    Update clock.cpp

--- ADT-Clock/clock.cpp
+++ ADT-Clock/clock.cpp
@@ -1,29 +1,58 @@
 #include "clock.h"
 
+using namespace std;
+
+/* membuat Clock baru, mengembalikan type Clock */
+Clock makeClock(int hh, int mm, int ss) {
+    Clock tClock;
+
+    tClock.HH = hh;
+    tClock.MM = mm;
+    tClock.SS = ss;
+
+    return tClock;
+}
+
+/* cek apakah jam yang dibuat valid atau tidak */
+bool isValid(int hh, int mm, int ss);
+
+/* mengembalikan nilai HH */
 Hour getHour(Clock c) {
     return c.HH;
 }
 
+/* mengembalikan nilai MM */
 Minute getMinute(Clock c) {
     return c.MM;
 }
 
+/* mengembalikan nilai SS */
 Second getSecond(Clock c) {
     return c.SS;
 }
 
-void setHour(Clock &c, int newHH) {
+/* set nilai HH */
+void setHour(Clock c, int newHH) {
     c.HH = newHH;
 }
 
-void setMinute(Clock &c, int newMM) {
+/* set nilai MM */
+void setMinute(Clock c, int newMM) {
     c.MM = newMM;
 }
 
-void setSecond(Clock &c, int newSS) {
+/* set nilai SS */
+void setSecond(Clock c, int newSS) {
     c.SS = newSS;
 };
 
-//bool isEqual(Clock c1, Clock c2);
-//Clock addClock(Clock c1, Clock c2);
-//void printClock(Clock c);
+/* cek apakah kedua jam bernilai sama */
+bool isEqual(Clock c1, Clock c2);
+
+/* tambahkan nilai Clock C1 dengan Clock C2 */
+Clock addClock(Clock c1, Clock c2);
+
+/* cetak jam */
+void printClock(Clock c) {
+    cout << c.HH << ":" << c.MM << ":" << c.SS << endl;
+}

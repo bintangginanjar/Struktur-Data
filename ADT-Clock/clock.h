0be3b75790395dbe053caa88970da83e16a01cd0
Bintang Ginanjar <bintang.ginanjar@gmail.com>
2021-09-27 22:20:20 +0700

    Add files via upload

---
+++ ADT-Clock/clock.h
@@ -0,0 +1,26 @@
+#ifndef CLOCK_H_INCLUDED
+#define CLOCK_H_INCLUDED
+
+typedef int Hour;
+typedef int Minute;
+typedef int Second;
+
+struct Clock {
+    Hour HH;
+    Minute MM;
+    Second SS;
+};
+
+Hour getHour(Clock c);
+Minute getMinute(Clock c);
+Second getSecond(Clock c);
+
+void setHour(Clock &c, int newHH);
+void setMinute(Clock &c, int newMM);
+void setSecond(Clock &c, int newSS);
+
+bool isEqual(Clock c1, Clock c2);
+Clock addClock(Clock c1, Clock c2);
+void printClock(Clock c);
+
+#endif // CLOCK_H_INCLUDED
